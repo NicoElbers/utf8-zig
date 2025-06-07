ddee95b0781c10d9dbfbdfff0e5b97e18818cb44
Nico Elbers <nico.b.elbers@gmail.com>
2025-06-07 14:49:22 +0200

    examples: Decoder example for nextStrict

--- examples/decoding.zig
+++ examples/decoding.zig
@@ -1,19 +1,39 @@
 pub fn main() !void {
-    const string = "こんにちは";
-    assert(string.len == 15);
+    const utf8_string = "こんにちは";
+    const invalid_string = "\xFFABC";
+    {
+        var decoder: utf8.Decoder = .init(utf8_string);
 
-    var decoder: utf8.Decoder = .init(string);
+        try exepectEqual('こ', decoder.next());
+        try exepectEqual('ん', decoder.next());
+        try exepectEqual('に', decoder.next());
+        try exepectEqual('ち', decoder.next());
+        try exepectEqual('は', decoder.next());
+        try exepectEqual(null, decoder.next());
+    }
+    {
+        var decoder: utf8.Decoder = .init(invalid_string);
 
-    try exepectEqual('こ', decoder.next());
-    try exepectEqual('ん', decoder.next());
-    try exepectEqual('に', decoder.next());
-    try exepectEqual('ち', decoder.next());
-    try exepectEqual('は', decoder.next());
-    try exepectEqual(null, decoder.next());
+        try exepectError(error.InvalidCodePoint, decoder.nextStrict());
+        try exepectEqual('A', decoder.nextStrict());
+        try exepectEqual('B', decoder.nextStrict());
+        try exepectEqual('C', decoder.nextStrict());
+        try exepectEqual(null, decoder.nextStrict());
+    }
+    {
+        var decoder: utf8.Decoder = .init(invalid_string);
+
+        try exepectEqual(0xFFFD, decoder.next());
+        try exepectEqual('A', decoder.next());
+        try exepectEqual('B', decoder.next());
+        try exepectEqual('C', decoder.next());
+        try exepectEqual(null, decoder.next());
+    }
 }
 
 const std = @import("std");
 const utf8 = @import("utf8");
 
 const exepectEqual = std.testing.expectEqual;
+const exepectError = std.testing.expectError;
 const assert = std.debug.assert;
